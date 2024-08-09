5c3ef376f4f83c42d11beacaf06dd3a872bb243e
Filip <filiptenerowicz@interia.pl>
2024-08-09 17:42:20 +0200

    add project_plan.py

--- project_plan.py
+++ project_plan.py
@@ -1,38 +1,17 @@
 
 # Interakcja z bazą danych: Po poprawnym hashowaniu hasła, dane użytkownika powinny zostać zapisane w bazie danych.
 
-#rejestracja.
+
 import bcrypt
-# to będzie klasa Sign_up ! , będzie zawierała validacje i 2 funckje : (sprawdzającą e maia i hasło)
-def sign_up():
-    email = input("Enter the e-mail: ")
-    pwd = input("""Set a password
-    : the password must include
-     1. minimum length of eight characters and maximum 20
-     2. use letters/digit/symbols combination for stronger password
+def hashing_password(password: str) -> str:
+    """hashing password during user sign_up"""
+    bytes_password = password.encode('utf-8')
+    hashed_password = bcrypt.hashpw(bytes_password, bcrypt.gensalt())
+    hashed_password_str = hashed_password.decode('utf-8')
+    return hashed_password_str
 
-    : Type password: """)
-    if len(pwd) > 8:
-        pass # DODAM FUNKCJĘ SPRAWDZAJĄCĄ - password_validation
-    conf_pwd = input("Confirm the password: ")
-
-    if pwd == conf_pwd:
-        #Konwertuje hasło na bajty
-        bytes_pwd = pwd.encode('utf-8')
-
-        # hashowanie hasła - generowanie salt i przekazanie jako argumentu.
-        hashed_pwd = bcrypt.hashpw(bytes_pwd, bcrypt.gensalt())
-
-        #Konwertowanie hash na str przed zapisem.
-        hashed_pwd_str = hashed_pwd.decode('utf-8')
-
-        with open("credentials.txt", "a") as f:
-            f.write(email + "\n")
-            f.write(hashed_pwd_str + "\n")
-
-        print("You have registred successfully!")
-    else:
-        print("Password is not same as above! \n")
-
-sign_up_user = sign_up()
-
+def save_credentials_to_file(email, hashed_password_str):
+    """ saves user's credentials during sign_up"""
+    with open("credentials.txt", "a") as f:
+        f.write(email + "\n")
+        f.write(hashed_password_str + "\n")
