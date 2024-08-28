f529efdd73f64f21bee95605905b2927b423d96a
Filip <filiptenerowicz@interia.pl>
2024-08-28 16:14:01 +0200

    feat: add SessionManager for handling SQLAlchemy sessions

---
+++ session_manager.py
@@ -0,0 +1,20 @@
+
+class SessionManager:
+    def __init__(self, session_factory):
+        self.session_factory = session_factory
+        self.session = None
+
+    def __enter__(self):
+        """Creating session during context enter"""
+        self.session = self.session_factory()
+        return self.session
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+
+        try:
+            if exc_type is not None:
+                self.session.rollback()
+            else:
+                self.session.commit()
+        finally:
+            self.session.close()
--- tables.py
+++ tables.py
@@ -2,9 +2,9 @@ from sqlalchemy import Column, String, Integer, DateTime, Numeric, Text, Foreign
 from sqlalchemy.orm import relationship, sessionmaker, declarative_base
 from sqlalchemy_utils import database_exists, create_database
 from datetime import datetime
+from session_manager import SessionManager
 
 Base = declarative_base()
-
 
 class Users(Base):
     __tablename__ = 'users'
@@ -56,52 +56,43 @@ if not database_exists(engine.url):
 # Tworzenie tabeli w bazie danych (jeśli nie istnieje)
 Base.metadata.create_all(engine)
 
-# Tworzenie sesji SQLAlchemy
+
 Session = sessionmaker(bind=engine)  # zwraca klasę - tworzącą sesję.
-session = Session()
 
-standard_categories = [
-    ("Food and Drinks", "Expenses on food and drinks", "#FF5733", "food_icon"),
-    ("Transport", "Expenses on transport", "#33FF57", "transport_icon"),
-    ("Housing", "Expenses on housing", "#3357FF", "housing_icon"),
-    ("Entertainment", "Expenses on entertainment", "#FF33A1", "entertainment_icon"),
-    ("Clothing and Shoes", "Expenses on clothing and shoes", "#FF5733", "clothing_icon"),
-    ("Health", "Expenses on health", "#33FFA1", "health_icon"),
-    ("Bills", "Expenses on bills", "#33A1FF", "bills_icon"),
-    ("Education", "Expenses on education", "#FFA133", "education_icon"),
-    ("Travel", "Expenses on travel", "#FF3333", "travel_icon"),
-    ("Shopping", "Expenses on shopping", "#33FF33", "shopping_icon"),
-    ("Other", "Other expenses", "#A1A1A1", "other_icon")
-]
+with SessionManager(Session) as session:
+    standard_categories = [
+        ("Food and Drinks", "Expenses on food and drinks", "#FF5733", "food_icon"),
+        ("Transport", "Expenses on transport", "#33FF57", "transport_icon"),
+        ("Housing", "Expenses on housing", "#3357FF", "housing_icon"),
+        ("Entertainment", "Expenses on entertainment", "#FF33A1", "entertainment_icon"),
+        ("Clothing and Shoes", "Expenses on clothing and shoes", "#FF5733", "clothing_icon"),
+        ("Health", "Expenses on health", "#33FFA1", "health_icon"),
+        ("Bills", "Expenses on bills", "#33A1FF", "bills_icon"),
+        ("Education", "Expenses on education", "#FFA133", "education_icon"),
+        ("Travel", "Expenses on travel", "#FF3333", "travel_icon"),
+        ("Shopping", "Expenses on shopping", "#33FF33", "shopping_icon"),
+        ("Other", "Other expenses", "#A1A1A1", "other_icon")
+    ]
 
+    category_objects = []
 
-category_objects = []
-for category in standard_categories:
+    for category in standard_categories:
 
-    existing_category = session.query(Categories).filter_by(
-        category_name=category[0],
-        colour=category[2],
-        icon=category[3]
-    ).first()
-
-    if not existing_category:
-        new_category = Categories(
+        existing_category = session.query(Categories).filter_by(
             category_name=category[0],
-            description=category[1],
             colour=category[2],
             icon=category[3]
-        )
-        category_objects.append(new_category)
+        ).first()
 
-if category_objects:
-    session.add_all(category_objects)
-    session.commit()
+        if not existing_category:
+            new_category = Categories(
+                category_name=category[0],
+                description=category[1],
+                colour=category[2],
+                icon=category[3]
+            )
+            category_objects.append(new_category)
 
-
-# print("Code for check tables:")
-# users = session.query(Users).all()
-# for user in users:
-#     print(f"\n email = {user.email} \n username = {user.username}")
-
-
+    if category_objects:
+        session.add_all(category_objects)
 
